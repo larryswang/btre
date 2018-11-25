b4dd39ca96eb46644c7500dfd4bbc4adb3a3785c
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-11-25 17:18:56 -0500

    fix a lot of bugs

--- listings/views.py
+++ listings/views.py
@@ -28,9 +28,41 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # bedroom
+    if 'bedroom' in request.GET:
+        bedroom = request.GET['bedroom']
+        if bedroom:
+            queryset_list = queryset_list.filter(bedroom__lte=bedroom)
+            
+    # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
     context = {        
         'state_choices' : state_choices,
         'bedroom_choices' : bedroom_choices,
-        'price_choices' : price_choices
+        'price_choices' : price_choices,
+        'listings': queryset_list
         }
     return render(request, 'listings/search.html', context)
